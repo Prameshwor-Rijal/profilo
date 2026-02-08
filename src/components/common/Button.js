290785d06aac87f13fb5a0d8904d57bca6607d54
Shadow <rijalprameshwor97@gmail.com>
2026-02-09 05:25:48 +0545

    Added login page from profilo repo

---
+++ src/components/common/Button.js
@@ -0,0 +1,34 @@
+/**
+ * Reusable Button Component
+ * Modular button component with different variants
+ */
+
+import React from 'react';
+import './Button.css';
+
+const Button = ({ 
+  children, 
+  variant = 'primary', 
+  size = 'medium', 
+  onClick, 
+  disabled = false,
+  type = 'button',
+  className = '',
+  ...props 
+}) => {
+  const buttonClasses = `btn btn-${variant} btn-${size} ${className}`.trim();
+
+  return (
+    <button
+      type={type}
+      className={buttonClasses}
+      onClick={onClick}
+      disabled={disabled}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
